6ee4a166791017617499e8e7969f014c3a8b115a
vsc-hvdc <zhouhangseu@outlook.com>
2019-04-23 21:33:32 -0700

    basic project structure

---
+++ src/compression/JPEG_demo.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib as plt
+
+def JPEG_compress(datastream):
+	pass
+	print("Nothing is compressed, this is just a demo of interface")
+
---
+++ src/utils.py
@@ -0,0 +1,1 @@
+"""utilities funcitons for project"""
